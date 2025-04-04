f98bb3b9d5568cc06acbec923fc922eaa01f218e
Lautaro Bonseñor <lbonsenor@itba.edu.ar>
2025-04-04 14:48:40 -0300

    :spark: Markdown Preview and prettier editor

--- init.lua
+++ init.lua
@@ -1029,5 +1029,11 @@ require('lazy').setup({
   },
 })
 
+local builtin = require 'telescope.builtin'
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
